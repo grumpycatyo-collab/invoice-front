83943ff44ffb823d501018e327642fa888529183
grumpycatyo-collab <linkolnmax24@gmail.com>
2024-09-28 09:57:53 +0300

    Minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
         href="https://vercel.com/templates/next.js/blob-starter"
         className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
       >
-        <p>Deploy your own to Vercel</p>
+        <p>See terms and conditions</p>
         <ExpandingArrow />
       </Link>
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-        Blob on Vercel
+        Invoice your contracts
       </h1>
       <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
         <Uploader />
@@ -37,7 +37,7 @@ export default function Home() {
         </Link>
         .
       </p>
-      <div className="sm:absolute sm:bottom-0 w-full px-20 py-10 flex justify-between">
+      {/* <div className="sm:absolute sm:bottom-0 w-full px-20 py-10 flex justify-between">
         <Link href="https://vercel.com">
           <Image
             src="/vercel.svg"
@@ -60,7 +60,7 @@ export default function Home() {
           />
           <p className="font-light">Source</p>
         </Link>
-      </div>
+      </div> */}
     </main>
   )
 }
